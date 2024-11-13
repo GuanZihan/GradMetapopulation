39d0a4d761820b2be2a6b4009895b9c5b17f65b9
GuanZihan <jmgzh123@outlook.com>
2024-11-13 00:33:33 -0500

    update scripts

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,8 +1,8 @@
-for i in 0
+for i in 1 5 9
 do
 target_week=$((49 + $i))
 python src/prepare_dataset.py --moving_window $i --week $target_week
 python src/prepare_dataset.py --moving_window $i --week $target_week --test
 python src/prcess_data_dung.py --moving_window $i --eps 1
-python src/main.py -st MA -j -d cpu --seed 2345 -m meta -di bogota -date "${i}_moving" -note "seed_2345"
+python src/main.py -st MA -j -d cpu --seed 2345 -m meta -di bogota -date "${i}_moving" -note "${i}_moving" --week $target_week
 done
